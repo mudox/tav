437379215ca7ab6a99b4b0bb5e22669d4d33f6e4
mudox <imudox@gmail.com>
2021-07-31 12:45:13 +0800

    remove `show_message`

--- src/tmux/cmd.rs
+++ src/tmux/cmd.rs
@@ -1,20 +1,6 @@
 use std::process::Command;
 
 use log::debug;
-use termion::cursor;
-
-pub fn show_message(msg: &str) {
-    Command::new("tmux")
-        .arg("display-popup")
-        .arg("-C") // close existing popup window
-        .arg("-w44")
-        .arg("-h3")
-        .arg("echo")
-        .arg("-n")
-        .arg(format!("{}{:^40}", cursor::Hide, msg))
-        .spawn()
-        .unwrap();
-}
 
 pub fn switch_to(target: &str) {
     debug!("switch to: {}", target);
