fd3ffc1007e93c19997515eb7b874c27f9ad36af
mudox <imudox@gmail.com>
2021-08-05 08:30:57 +0800

    logging: use `println` in test

--- src/logging.rs
+++ src/logging.rs
@@ -1,7 +1,15 @@
-use log::error;
+#[cfg(not(test))]
+#[allow(unused_imports)]
+pub use log::{debug, error, info, trace, warn};
+
+#[cfg(test)]
+#[allow(unused_imports)]
+pub use std::{
+    println as info, println as warn, println as debug, println as error, println as trace,
+};
 
 pub fn init() {
-    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_LOG", "trace");
     pretty_env_logger::init();
 
     std::panic::set_hook(Box::new(|info| {
